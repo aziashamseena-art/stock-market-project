e3e7b8e5a0557c93486805c065fc5f0874f42c81
azia <aziashamseena@gmail.com>
2026-09-02 12:17:39 +0530

    first upload

---
+++ app.py
@@ -0,0 +1,20 @@
+# stock investment tool
+
+print("welcome to the stock investment tool!")
+
+age = int(input("Please enter your age: "))
+
+if age < 18:
+    print("Sorry, you must be at least 18 years old to use this tool.")
+elif age >= 18 and age < 20:
+    equity=90;large_cap=50;mid_cap=50;small_cap=25;debt=5;gold=5;
+elif age >= 20 and age < 30:
+    equity=80;large_cap=50;mid_cap=20;small_cap=10;debt=15;gold=5;
+elif age >= 30 and age < 40:
+    equity=70;large_cap=45;mid_cap=15;small_cap=10;debt=20;gold=10;
+elif age >= 40 and age < 50:
+    equity=60;large_cap=40;mid_cap=15;small_cap=15;debt=30;gold=10;
+elif age >= 50 and age < 60:
+    equity=50;large_cap=35;mid_cap=10;small_cap=5;debt=40;gold=10;
+else:
+    equity=30;large_cap=25;mid_cap=5;small_cap=0;debt=60;gold=10;
